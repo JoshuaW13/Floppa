e33418f05f87c52c6cf2706857e8bf2e27facea4
JoshuaW13 <josh.walters@shaw.ca>
2023-09-10 09:01:59 -0600

    Updated Player jump

--- Actors/Player/PLayer.gd
+++ Actors/Player/PLayer.gd
@@ -2,7 +2,7 @@ class_name Player
 extends Actor
 
 #constant max player speed
-const PLAYER_SPEED = Vector2(175, 500);
+const PLAYER_SPEED = Vector2(175, 450);
 
 #signals
 signal health_update(health)
@@ -59,7 +59,7 @@ func set_knock():
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft"),
-		-1.0 if (Input.is_action_just_pressed("jump") and is_on_floor()) or knock else 1.0
+		-1.0 if (Input.is_action_just_pressed("jump") and is_on_floor()) or knock else 1.5
 		)
 
 #calculate move velocity based on direction and if jump is interrupted dont allow double jumps
